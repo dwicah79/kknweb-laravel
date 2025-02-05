ddd11109bc229d969fe20ca1decbcbf5e9ff7172
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-05 22:16:55 +0700

    install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,14 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
                 playfair: ["Playfair Display", "serif"],
+                primary2: ["Bebas Neue", "sans-serif"],
             },
         },
     },
