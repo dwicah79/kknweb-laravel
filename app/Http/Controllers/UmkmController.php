b50554c3d240f6dba4d008c8366522be488dbf55
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-05 22:17:10 +0700

    add umkm controller to make business logic

---
+++ app/Http/Controllers/UmkmController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Umkm;
+use Illuminate\Http\Request;
+
+class UmkmController extends Controller
+{
+    public function index()
+    {
+        $data = Umkm::paginate(10);
+        return view('dashboard.umkm.index', compact('data'));
+    }
+}
