02bca806ee481bd3741b26bde956db606ef9e876
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-07 01:25:16 +0700

    add route login,createumkm,delete

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\UmkmController;
+use UniSharp\LaravelFilemanager\Lfm;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UmkmController;
+use App\Http\Controllers\CKEditorCOntroller;
+use App\Http\Controllers\DashboardController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
 
-route::get('/', [DashboardController::class, 'index']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/umkm', [UmkmController::class, 'index'])->name('umkm.index');
+    Route::get('/umkm/create', [UmkmController::class, 'create'])->name('umkm.create');
+    Route::post('/umkm/create', [UmkmController::class, 'store'])->name('umkm.store');
+    Route::post('/ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.upload');
+    Route::delete('/umkm/{id}', [UmkmController::class, 'destroy'])->name('umkm.destroy');
+});
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::view('/dashboard', 'dashboard.index')->name('dashboard');
-// Route::view('/umkm', 'dashboard.umkm.index')->name('dashboard');
-Route::get('/umkm', [UmkmController::class, 'index'])->name('umkm.index');
-Route::get('/umkm/create', [UmkmController::class, 'create'])->name('umkm.create');
 
 
-Route::view('/login', 'auth.login')->name('login');
