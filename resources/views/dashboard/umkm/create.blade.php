7edec213bb6c880784a78573dd1a922e2445b062
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-07 01:41:40 +0700

    change button submit in umkm create

--- resources/views/dashboard/umkm/create.blade.php
+++ resources/views/dashboard/umkm/create.blade.php
@@ -11,27 +11,27 @@
         <form action="{{ route('umkm.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="flex flex-wrap w-full">
-                <div class="mb-5 w-1/2 px-5">
+                <div class="mb-5 w-full md:w-1/2 px-5">
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Nama UMKM <span
                             class="text-red-500">*</span></label>
                     <input type="text" id="name" name="name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:focus:ring-green-500 dark:focus:border-green-500"
                         placeholder="Nama UMKM..." required />
                 </div>
-                <div class="mb-5 w-1/2 px-5">
+                <div class="mb-5 w-full md:w-1/2 px-5">
                     <label for="telp" class="block mb-2 text-sm font-medium text-gray-900 ">No Telp. <span
                             class="text-red-500">*</span></label>
                     <input type="number" id="telp" name="telp"
                         class="bg-gray-50 no-spinner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:focus:ring-green-500 dark:focus:border-green-500"
                         required placeholder="Nomor Bisnis/Pesanan" />
                 </div>
-                <div class="mb-5 px-5 w-1/2">
+                <div class="mb-5 px-5 w-full md:w-1/2">
                     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat
                         <span class="text-red-500">*</span></label>
                     <textarea name="address"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
                 </div>
-                <div class="mb-5 px-5 w-1/2">
+                <div class="mb-5 px-5 w-full md:w-1/2">
                     <label for="image" class="block mb-2 text-sm font-medium text-gray-900 ">Gambar <span
                             class="text-red-500">*</span></label>
                     <input type="file" class="filepond" id="image" name="image" required>
@@ -42,8 +42,10 @@
                     <textarea name="description" id="konten" cols="30" rows="10"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
                 </div>
+                <div class="w-full flex justify-end px-5">
+                    <button class="btn-primmary w-full md:w-fit" type="submit">Kirim</button>
+                </div>
             </div>
-            <button type="submit" class="btn-primmary">Kirim</button>
         </form>
     </div>
 
