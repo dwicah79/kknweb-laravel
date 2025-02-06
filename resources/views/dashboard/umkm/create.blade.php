eb784ac7b4f879bdd149bd6ce9163423037bfdc9
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-07 01:24:37 +0700

    add create view and process umkm

--- resources/views/dashboard/umkm/create.blade.php
+++ resources/views/dashboard/umkm/create.blade.php
@@ -1,45 +1,64 @@
 @extends('template.sidebar')
 @section('konten')
     <x-breadcrumb :links="[
-        ['name' => 'DASHBOARD', 'url' => route('dashboard')],
+        ['name' => 'DASHBOARD', 'url' => route('dashboard.index')],
         ['name' => 'UMKM', 'url' => route('umkm.index')],
         ['name' => 'TAMBAH DATA UMKM', 'url' => '#'],
     ]" />
 
     <div class="rounded-lg bg-white p-10">
-        <form class="max-w-full mx-auto flex flex-wrap">
-            <div class="mb-5 w-1/2 px-5">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Nama UMKM <span
-                        class="text-red-500">*</span></label>
-                <input type="text" id="name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5     dark:focus:ring-green-500 dark:focus:border-green-500"
-                    placeholder="Nama UMKM..." required />
+        <x-alert type="error" />
+        <form action="{{ route('umkm.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="flex flex-wrap w-full">
+                <div class="mb-5 w-1/2 px-5">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Nama UMKM <span
+                            class="text-red-500">*</span></label>
+                    <input type="text" id="name" name="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:focus:ring-green-500 dark:focus:border-green-500"
+                        placeholder="Nama UMKM..." required />
+                </div>
+                <div class="mb-5 w-1/2 px-5">
+                    <label for="telp" class="block mb-2 text-sm font-medium text-gray-900 ">No Telp. <span
+                            class="text-red-500">*</span></label>
+                    <input type="number" id="telp" name="telp"
+                        class="bg-gray-50 no-spinner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:focus:ring-green-500 dark:focus:border-green-500"
+                        required placeholder="Nomor Bisnis/Pesanan" />
+                </div>
+                <div class="mb-5 px-5 w-1/2">
+                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat
+                        <span class="text-red-500">*</span></label>
+                    <textarea name="address"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                </div>
+                <div class="mb-5 px-5 w-1/2">
+                    <label for="image" class="block mb-2 text-sm font-medium text-gray-900 ">Gambar <span
+                            class="text-red-500">*</span></label>
+                    <input type="file" class="filepond" id="image" name="image" required>
+                </div>
+                <div class="mb-5 px-5 w-full">
+                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Konten
+                        UMKM <span class="text-red-500">*</span></label>
+                    <textarea name="description" id="konten" cols="30" rows="10"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                </div>
             </div>
-            <div class="mb-5 w-1/2 px-5">
-                <label for="telp" class="block mb-2 text-sm font-medium text-gray-900 ">No Telp. <span
-                        class="text-red-500">*</span></label>
-                <input type="number" id="telp"
-                    class="bg-gray-50 no-spinner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5     dark:focus:ring-green-500 dark:focus:border-green-500"
-                    required placeholder="Nomor Bisnis/Pesanan" />
-            </div>
-            <div class="mb-5 w-full px-5">
-                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 ">Deskripsi <span
-                        class="text-red-500">*</span></label>
-                <textarea id="description"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5     dark:focus:ring-green-500 dark:focus:border-green-500"
-                    required></textarea>
-            </div>
-            <div class="mb-5 px-5 w-full">
-                <label for="gambar" class="block mb-2 text-sm font-medium text-gray-900 ">Gambar <span
-                        class="text-red-500">*</span></label>
-                <input type="file" class="filepond" id="gambar" name="gambar" required>
-            </div>
-            <div class="w-full flex px-5 justify-end">
-                <a href="" class="btn-primmary">Kirim</a>
-            </div>
+            <button type="submit" class="btn-primmary">Kirim</button>
         </form>
     </div>
 
+    <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+    <script>
+        ClassicEditor
+            .create(document.querySelector('#konten'), {
+                ckfinder: {
+                    uploadUrl: '/laravel-filemanager/upload?type=Files&_token={{ csrf_token() }}'
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
 
     <script>
         FilePond.registerPlugin(FilePondPluginImagePreview);
@@ -62,24 +81,5 @@
                 document.querySelector('#image').value = ''; // Clear the input value on file removal
             },
         });
-    </script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
-    <script>
-        ClassicEditor
-            .create(document.querySelector('#description'), {
-                toolbar: [
-                    'heading', '|',
-                    'bold', 'italic', 'underline', '|',
-                    'link', 'bulletedList', 'numberedList', 'blockQuote', '|',
-                    'imageUpload', 'mediaEmbed', 'insertTable', '|',
-                    'undo', 'redo'
-                ],
-                ckfinder: {
-                    uploadUrl: '/laravel-filemanager/upload?type=Files&_token={{ csrf_token() }}'
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
     </script>
 @endsection
