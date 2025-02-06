26fabc5c785a3459a0e7132f35370ac09d486059
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-07 02:01:40 +0700

    add edit and update route

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/umkm', [UmkmController::class, 'index'])->name('umkm.index');
     Route::get('/umkm/create', [UmkmController::class, 'create'])->name('umkm.create');
     Route::post('/umkm/create', [UmkmController::class, 'store'])->name('umkm.store');
-    Route::post('/ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.upload');
+    // Route::post('/ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.upload');
     Route::delete('/umkm/{id}', [UmkmController::class, 'destroy'])->name('umkm.destroy');
+    Route::get('/umkm/{id}/edit', [UmkmController::class, 'edit'])->name('umkm.edit');
+    Route::put('/umkm/{id}', [UmkmController::class, 'update'])->name('umkm.update');
 });
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
