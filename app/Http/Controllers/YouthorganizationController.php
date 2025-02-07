5a8c657feeb4a4acf53b6d91ac536c959df79bd7
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-07 19:43:09 +0700

    add logic youth organization

---
+++ app/Http/Controllers/YouthorganizationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Position;
+use App\Models\Youth_Organization;
+use Illuminate\Http\Request;
+
+class YouthorganizationController extends Controller
+{
+    public function index()
+    {
+        $data = Youth_Organization::with('village')->paginate(10);
+        // return $data;
+        return view('dashboard.youth-organization.index', compact('data'));
+    }
+
+    public function create()
+    {
+        $data = Position::all();
+
+        return view('dashboard.youth-organization.create', compact('data'));
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'name' => 'required',
+            'position' => 'required|exists:positions,id',
+            'telp' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+
+
+        try {
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $fileName = time() . '_' . $file->getClientOriginalName();
+                $file->move(public_path('uploads'), $fileName);
+
+                $imagePath = 'uploads/' . $fileName;
+            } else {
+                return redirect()->back()->withErrors(['image' => 'Image is required']);
+            }
+
+            Youth_Organization::create([
+                'name' => $request->name,
+                'telp' => $request->telp,
+                'job_title_id' => $request->position,
+                'image' => $imagePath,
+            ]);
+
+            return redirect()->route('youth-organization.index')->with('success', 'Data berhasil ditambahkan');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+}
