d1283fe6bec09bd0d9b3c2e648f5da13a8c6e457
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-10 01:40:44 +0700

    add edit logic youth organization

--- app/Http/Controllers/YouthorganizationController.php
+++ app/Http/Controllers/YouthorganizationController.php
@@ -58,4 +58,54 @@ class YouthorganizationController extends Controller
         }
     }
 
+    public function edit($id)
+    {
+        $data = Youth_Organization::find($id);
+        $position = Position::all();
+        return view('dashboard.youth-organization.detail', compact('data', 'position'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'position' => 'required|exists:positions,id',
+            'telp' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        try {
+            $data = Youth_Organization::find($id);
+
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $fileName = time() . '_' . $file->getClientOriginalName();
+                $file->move(public_path('uploads'), $fileName);
+
+                $imagePath = 'uploads/' . $fileName;
+            } else {
+                $imagePath = $data->image;
+            }
+
+            $data->update([
+                'name' => $request->name,
+                'telp' => $request->telp,
+                'job_title_id' => $request->position,
+                'image' => $imagePath,
+            ]);
+
+            return redirect()->route('youth-organization.index')->with('success', 'Data berhasil diubah');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $data = Youth_Organization::find($id);
+        $data->delete();
+
+        return redirect()->route('village.index')->with('success', 'Data berhasil dihapus');
+    }
+
 }
