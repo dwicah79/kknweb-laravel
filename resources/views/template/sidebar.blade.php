3aab91f42c0449f3d108c6e023960cfcaf78ca73
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-10 02:18:02 +0700

    add route pkk organization in sidebar

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -125,8 +125,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href=""
-                        class="flex items-center p-2 {{ Request::is('client') ? ' text-white bg-green-500 hover:bg-green-600 transition duration-300' : '' }} text-gray-900 rounded-lg  hover:bg-green-200 dark:hover:bg-green-500 group">
+                    <a href="{{ route('pkk.index') }}"
+                        class="flex items-center p-2 {{ in_array(Request::path(), ['PKK-organization', 'PKK-organization/create']) ? ' text-white bg-green-500 hover:bg-green-600 transition duration-300' : '' }} text-gray-900 rounded-lg  hover:bg-green-200 dark:hover:bg-green-500 group">
                         <i class="fa-solid fa-people-line"></i>
 
                         <span class="flex-1 ms-3 whitespace-nowrap">Struktur PKK</span>
