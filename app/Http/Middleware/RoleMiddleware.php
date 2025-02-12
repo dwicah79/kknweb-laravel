417e15411410b4cad613f77b17380cbb94d55e25
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-13 03:04:17 +0700

    change toast error hak akses

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -15,7 +15,7 @@ class RoleMiddleware
     public function handle(Request $request, Closure $next, $role): Response
     {
         if (!auth()->check() || !auth()->user()->hasRole($role)) {
-            Alert::error('Error', 'Anda Tidak Memiliki Akses Ke Halaman Ini !!!');
+            Alert::error('Akses Ditolak !!!', 'Anda Tidak Memiliki Akses Ke Halaman Ini');
 
             return redirect()->route('dashboard.index')->with('error', 'Anda Tidak Memiliki Akses Ke Halaman Ini !!!');
         }
