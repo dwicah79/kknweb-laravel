37cfb9805e7020ad115ea144c147ce1a199e048f
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-17 02:36:32 +0700

    create home controller logic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\Speech;
+use App\Models\Village_organization;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,10 +12,26 @@ class HomeController extends Controller
     public function index()
     {
         $slides = About::select('title', 'image')->get()->map(function ($slide) {
-            $slide->image = asset($slide->image); // Buat URL absolut
+            $slide->image = asset($slide->image);
             return $slide;
         });
 
-        return view('home.index', compact('slides'));
+        $village_organization = Village_organization::with('position')
+            ->whereHas('position', function ($query) {
+                $query->where('job_title', 'Kepala Dusun');
+            })
+            ->get(); // Mengambil semua data sebagai collection
+
+        $headvillage = $village_organization->first();
+        $speech = Speech::select('speech')->first();
+
+        $data = [
+            'headvillage' => $headvillage, // Ubah ke single object, bukan collection
+            'photo' => $headvillage ? asset($headvillage->image) : null, // Hindari error jika null
+            'speech' => $speech ? $speech->speech : null,
+        ];
+
+        return view('home.index', compact('slides'), $data);
+
     }
 }
