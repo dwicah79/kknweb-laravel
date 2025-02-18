55c0135057bd9578cee7798a22247c6fa0cb5488
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-18 19:34:22 +0700

    create view news

--- resources/views/dashboard/news/index.blade.php
+++ resources/views/dashboard/news/index.blade.php
@@ -1,13 +1,7 @@
 @extends('template.sidebar')
 @section('konten')
     <x-breadcrumb :links="[['name' => 'DASHBOARD', 'url' => route('dashboard.index')], ['name' => 'BERITA', 'url' => '#']]" />
-    <div class="w-full flex justify-between items-center bg-gradient-primmary px-5 py-5 mb-2">
-        <h1 class="font-extrabold uppercase text-white text-xl">BERITA</h1>
-        <a href="{{ route('news.create') }}" class="bg-white rounded-lg w-fit whitespace-nowrap p-2 hover:bg-slate-100"><i
-                class="fa-solid fa-plus"></i>
-            Tambah</a>
-    </div>
-
+    <x-header-component title="BERITA DUSUN" route="news.create" />
     <div class="bg-white shadow-md sm:rounded-lg overflow-hidden p-6">
         <div class="w-full flex justify-end ">
             <div class="w-full md:w-1/2">
