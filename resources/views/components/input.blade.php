b50d71b86c4ab8c6660e580ae3b23898770d9076
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-20 12:17:28 +0700

    change focus form input component color

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -5,5 +5,5 @@
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $label }} <span
             class="text-red-500">*</span></label>
     <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}"
-        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-600 dark:focus:border-green-600">
+        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-600 dark:focus:border-primary-600">
 </div>
