6c96a0e990eefbc89309b642af9a31ea3c83057e
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-21 21:41:54 +0700

    add view index pkk in home

---
+++ resources/views/home/PKK/index.blade.php
@@ -0,0 +1,47 @@
+@extends('template.home')
+@section('content')
+    <section class="mt-24 md:mx:auto md:px-20">
+        <x-breadcrumb :links="[['name' => 'Home', 'url' => '/'], ['name' => 'STRUKTUR PKK', 'url' => '#']]" />
+        <div class="container mx-auto px-5 ">
+            <div class="w-full mb-8 text-start" data-aos="fade-up" data-aos-delay="200">
+                <h1 class="uppercase text-3xl md:text-5xl text-primary-500 font-extrabold leading-tight">
+                    STRUKTUR PKK DUSUN KRETEK 1
+                </h1>
+                <p class="font-semibold">Menyajikan informasi terkait organisasi PKK di Dusun Kretek 1</p>
+            </div>
+
+            <div class="hidden md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
+                @foreach ($pkk as $person)
+                    <div class="bg-white rounded-2xl shadow-lg overflow-hidden text-center" data-aos="fade-up">
+                        <img src="{{ asset($person->image) }}" alt="{{ $person->name }}" loading="lazy"
+                            class="w-full h-64 object-cover bg-primary-500">
+                        <div class="p-4 bg-primary-500 text-white">
+                            <h2 class="font-bold text-lg">{{ $person->name }}</h2>
+                            <p class="text-sm">{{ $person->village->job_title }}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+
+            <div class="block md:hidden">
+                <div class="owl-carousel sotk-slider">
+                    @foreach ($pkk as $person)
+                        <div class="item bg-white rounded-2xl shadow-lg overflow-hidden text-center">
+                            <img src="{{ asset($person->image) }}" alt="{{ $person->name }}" loading="lazy"
+                                class="w-full h-64 object-cover bg-primary-500">
+                            <div class="p-4 bg-primary-500 text-white">
+                                <h2 class="font-bold text-lg">{{ $person->name }}</h2>
+                                <p class="text-sm">{{ $person->village->job_title }}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="hidden md:flex justify-center mb-5">
+            {{ $pkk->links('vendor.pagination.custom') }}
+        </div>
+    </section>
+@endsection
